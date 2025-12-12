f2c91406d0a063e9984b438753253f69a25d3aa0
mentix02 <manan.yadav02@gmail.com>
2025-12-13 00:37:24 +0530

    Implement /dashboard

--- ui/app/dashboard/page.tsx
+++ ui/app/dashboard/page.tsx
@@ -1,6 +1,10 @@
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+
 import { auth } from "@/lib/auth";
 
 export default async function Page() {
@@ -8,5 +12,24 @@ export default async function Page() {
 
   if (!session) redirect("/login?next=/dashboard");
 
-  return <div>Dashboard</div>;
+  const user = session.user;
+
+  return (
+    <Grid container alignItems="center" justifyContent="center" minHeight="90dvh">
+      <Grid size={{ xs: 8, md: 8, lg: 6 }}>
+        <Paper
+          elevation={3}
+          sx={{
+            p: { xs: 3, sm: 4 },
+            borderRadius: 3,
+            boxShadow: 4,
+          }}
+        >
+          <Typography variant="h5" component="h1" align="center">
+            Welcome to your dashboard, {user.name}!
+          </Typography>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
 }
