ecd07f14a135003f33564aea071bb8b8568a6341
mentix02 <manan.yadav02@gmail.com>
2025-12-13 00:32:07 +0530

    Update userTable with new changes.
    - image is now a text()
    - set empty string for password
    - change ordering of some fields

--- ui/lib/database.ts
+++ ui/lib/database.ts
@@ -1,23 +1,23 @@
 import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { varchar, bigint, pgTable, boolean, timestamp } from "drizzle-orm/pg-core";
+import { text, varchar, bigint, pgTable, boolean, timestamp } from "drizzle-orm/pg-core";
 
 export const pgPool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 const db = drizzle({ client: pgPool });
 
 export const userTable = pgTable("user_user", {
-  image: varchar({ length: 200 }),
+  image: text(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 254 }).notNull(),
-  password: varchar({ length: 128 }).notNull(),
+  password: varchar({ length: 128 }).notNull().default(""),
 
   is_staff: boolean().notNull().default(false),
   is_active: boolean().notNull().default(true),
   is_superuser: boolean().notNull().default(false),
   email_verified: boolean()
-    .notNull()
-    .$default(() => false),
+    .$default(() => false)
+    .notNull(),
 
   last_login: timestamp({ withTimezone: true }),
   updated_at: timestamp({ withTimezone: true })
