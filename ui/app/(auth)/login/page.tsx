0cc43d8fee663b2c672a5fb4deeb6cd43871ffcb
mentix02 <manan.yadav02@gmail.com>
2025-12-13 00:32:43 +0530

    Set metadata for login form.

--- ui/app/(auth)/login/page.tsx
+++ ui/app/(auth)/login/page.tsx
@@ -1,8 +1,13 @@
+import { Metadata } from "next";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 import { auth } from "@/lib/auth";
 import LoginForm from "@/components/LoginForm";
+
+export const metadata: Metadata = {
+  title: "Login",
+};
 
 export default async function Page() {
   const session = await auth.api.getSession({ headers: await headers() });
