5efe2d6b6b5ba24a2788e45a608e0e0ef760c882
mentix02 <manan.yadav02@gmail.com>
2025-12-13 00:36:02 +0530

    Implement SignupForm in /signup

--- ui/app/(auth)/signup/page.tsx
+++ ui/app/(auth)/signup/page.tsx
@@ -1,3 +1,18 @@
+import { Metadata } from "next";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+import SignupForm from "@/components/SignupForm";
+
+export const metadata: Metadata = {
+  title: "Sign Up",
+};
+
 export default async function Page() {
-  return <div>Sign Up</div>;
+  const session = await auth.api.getSession({ headers: await headers() });
+
+  if (session) redirect("/dashboard");
+
+  return <SignupForm />;
 }
