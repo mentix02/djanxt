822311bd8eedca478e88333694f31330e8cfff0d
mentix02 <manan.yadav02@gmail.com>
2025-12-13 00:37:02 +0530

    Infer session type in authClient

--- ui/lib/auth-client.ts
+++ ui/lib/auth-client.ts
@@ -1,7 +1,14 @@
 import { createAuthClient } from "better-auth/react";
+import { inferAdditionalFields } from "better-auth/client/plugins";
 
-const authClient = createAuthClient({});
+import { auth } from "@/lib/auth";
+
+const authClient = createAuthClient({
+  plugins: [inferAdditionalFields<typeof auth>()],
+});
 
 export const { useSession, signIn, signOut, signUp } = authClient;
+
+export type Session = typeof authClient.$Infer.Session;
 
 export default authClient;
