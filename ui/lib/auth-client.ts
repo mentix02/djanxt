a55e1a1cbda5fb8e0903b57e9c7c2695a4839401
mentix02 <manan.yadav02@gmail.com>
2025-12-13 23:08:55 +0530

    Optional onetap commit added.

--- ui/lib/auth-client.ts
+++ ui/lib/auth-client.ts
@@ -1,10 +1,19 @@
 import { createAuthClient } from "better-auth/react";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 
+// Plugins
+// import { oneTapClient } from "better-auth/client/plugins";
+
 import { auth } from "@/lib/auth";
 
+// const GOOGLE_CLIENT_ID = "";
+
 const authClient = createAuthClient({
-  plugins: [inferAdditionalFields<typeof auth>()],
+  plugins: [
+    inferAdditionalFields<typeof auth>(),
+    // Uncomment to enable one tap sign-in by Google
+    // oneTapClient({ clientId: GOOGLE_CLIENT_ID }),
+  ],
 });
 
 export const { useSession, signIn, signOut, signUp } = authClient;
