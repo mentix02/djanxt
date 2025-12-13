cc32712dfb6fceaf1d6171a19bdd1dd96412d3d8
mentix02 <manan.yadav02@gmail.com>
2025-12-13 21:49:09 +0530

    Use custom argon2 hasher

--- djanxt/settings.py
+++ djanxt/settings.py
@@ -70,6 +70,14 @@ DATABASES = {
     'default': env.db(),
 }
 
+PASSWORD_HASHERS = [
+    "apps.user.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+    "django.contrib.auth.hashers.ScryptPasswordHasher",
+]
+
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
