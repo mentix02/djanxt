38f37e4f9378eafc355b7927a132c9ea019cc3db
mentix02 <manan.yadav02@gmail.com>
2025-12-15 22:23:18 +0530

    Added UserAdmin

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from apps.user.models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+
+    ordering = ('name',)
+    list_filter = ('email_verified',)
+    search_fields = ('name', 'email')
+    list_display = ('name', 'email', 'is_staff', 'email_verified')
+    readonly_fields = ('last_login', 'access_key', 'avatar', 'image')
+
+    def avatar(self, obj: User) -> str:
+        if obj.image:
+            return format_html('<img src="{src}" style="width: 250px; height: 250px;" />', src=obj.image)
+        return format_html('<span style="color: red;">{msg}</span>', msg=_('No avatar'))
+
+    avatar.short_description = _('Avatar')
+
+    add_fieldsets = (
+        (
+            None,
+            {'classes': ('wide',), 'fields': ('email', 'usable_password', 'password1', 'password2')},
+        ),
+    )
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('name',)}),
+        (_('Image'), {'fields': ('image', 'avatar')}),
+        (_('Credentials'), {'fields': ('access_key',)}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
