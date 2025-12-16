ea7d588ea4b389641ba608343ec64f1f53d1f41b
mentix02 <manan.yadav02@gmail.com>
2025-12-17 04:39:32 +0530

    Remove OneTap configs.

--- ui/lib/auth-client.ts
+++ ui/lib/auth-client.ts
@@ -3,19 +3,10 @@ import { customSessionClient } from "better-auth/client/plugins";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 
 // Plugins
-// import { oneTapClient } from "better-auth/client/plugins";
-
 import type { auth } from "@/lib/auth";
 
-// const GOOGLE_CLIENT_ID = "";
-
 const authClient = createAuthClient({
-  plugins: [
-    inferAdditionalFields<typeof auth>(),
-    customSessionClient<typeof auth>(),
-    // Uncomment to enable one tap sign-in by Google
-    // oneTapClient({ clientId: GOOGLE_CLIENT_ID }),
-  ],
+  plugins: [inferAdditionalFields<typeof auth>(), customSessionClient<typeof auth>()],
 });
 
 export const { useSession, signIn, signOut, signUp } = authClient;
