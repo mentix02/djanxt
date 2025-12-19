edd9bf2bba68b64d9cdac0c5cf10aa68667923d0
mentix02 <manan.yadav02@gmail.com>
2025-12-19 12:38:08 +0530

    Move links as inline arrays.

--- ui/components/layout/BottomNavbar.tsx
+++ ui/components/layout/BottomNavbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { redirect, usePathname } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
 import Box from "@mui/material/Box";
 import LinearProgress from "@mui/material/LinearProgress";
@@ -17,51 +17,15 @@ import AccountCircle from "@mui/icons-material/AccountCircle";
 import Link from "@/components/Link";
 import { signOut, useSession } from "@/lib/auth-client";
 
-const signedInLinks = [
-  <BottomNavigationAction
-    key="/tasks"
-    value="/tasks"
-    label="Tasks"
-    href="/tasks"
-    component={Link}
-    icon={<FormatListBulletedIcon />}
-  />,
-  <BottomNavigationAction
-    key="/account"
-    label="Account"
-    href="/account"
-    value="/account"
-    component={Link}
-    icon={<AccountCircle />}
-  />,
-  <BottomNavigationAction
-    key="/logout"
-    value="/logout"
-    label="Logout"
-    icon={<Logout />}
-    onClick={async () => {
-      await signOut();
-      redirect("/login");
-    }}
-  />,
-];
-
-const signedOutLinks = [
-  <BottomNavigationAction key="/" value="/" component={Link} label="Home" icon={<Home />} href="/" />,
-  <BottomNavigationAction key="/login" value="/login" component={Link} label="Login" icon={<Key />} href="/login" />,
-  <BottomNavigationAction
-    key="/signup"
-    href="/signup"
-    label="Signup"
-    value="/signup"
-    component={Link}
-    icon={<PersonAdd />}
-  />,
-];
-
 export default function BottomNavbar() {
+  const router = useRouter();
   const pathname = usePathname();
   const { isPending, data: session } = useSession();
+
+  const handleSignOut = async () => {
+    await signOut();
+    router.push("/login");
+  };
 
   if (isPending) {
     return <LinearProgress />;
@@ -70,7 +34,51 @@ export default function BottomNavbar() {
   return (
     <Box>
       <BottomNavigation showLabels value={pathname}>
-        {session ? signedInLinks : signedOutLinks}
+        {session
+          ? [
+              <BottomNavigationAction
+                key="/tasks"
+                value="/tasks"
+                label="Tasks"
+                href="/tasks"
+                component={Link}
+                icon={<FormatListBulletedIcon />}
+              />,
+              <BottomNavigationAction
+                key="/account"
+                label="Account"
+                href="/account"
+                value="/account"
+                component={Link}
+                icon={<AccountCircle />}
+              />,
+              <BottomNavigationAction
+                key="/logout"
+                value="/logout"
+                label="Logout"
+                icon={<Logout />}
+                onClick={handleSignOut}
+              />,
+            ]
+          : [
+              <BottomNavigationAction key="/" value="/" component={Link} label="Home" icon={<Home />} href="/" />,
+              <BottomNavigationAction
+                key="/login"
+                value="/login"
+                component={Link}
+                label="Login"
+                icon={<Key />}
+                href="/login"
+              />,
+              <BottomNavigationAction
+                key="/signup"
+                href="/signup"
+                label="Signup"
+                value="/signup"
+                component={Link}
+                icon={<PersonAdd />}
+              />,
+            ]}
       </BottomNavigation>
     </Box>
   );
